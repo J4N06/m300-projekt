da4df39752e84c17b8e96ce5d3397a6a550ca2a2
J4N06 <jan.birchler@iseag.ch>
2025-05-28 09:20:15 +0200

    main angepasst

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,20 +1,17 @@
-provider "aws" {
-  region = "us-east-1" # Oder die Region aus deinem Learner Lab
-}
-
 module "eks" {
   source          = "terraform-aws-modules/eks/aws"
+  version         = "20.8.4" # oder die aktuelle, bekannte funktionierende Version
   cluster_name    = "my-k8s-cluster"
   cluster_version = "1.27"
   subnet_ids      = module.vpc.public_subnets
   vpc_id          = module.vpc.vpc_id
 
-node_groups = {
-  default = {
-    desired_capacity = 2
-    max_capacity     = 2
-    min_capacity     = 2
-    instance_types   = ["t3.medium"]
+  eks_managed_node_groups = {
+    default = {
+      min_size     = 2
+      max_size     = 2
+      desired_size = 2
+      instance_types = ["t3.medium"]
+    }
   }
-}
 }
