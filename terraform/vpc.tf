1d99984ef19e6cb1dc6ee53019f8332c6ab3eb92
J4N06 <jan.birchler@iseag.ch>
2025-06-03 15:40:06 +0200

    teatt

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,21 +1,10 @@
 resource "aws_vpc" "main" {
   cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
   enable_dns_hostnames = true
 
   tags = {
     Name = "main-vpc"
-  }
-}
-
-resource "aws_subnet" "public" {
-  count                   = 1
-  cidr_block              = "10.0.1.0/24"
-  vpc_id                  = aws_vpc.main.id
-  map_public_ip_on_launch = true
-  availability_zone       = "us-east-1a"
-
-  tags = {
-    Name = "public-subnet-${count.index}"
   }
 }
 
@@ -24,6 +13,18 @@ resource "aws_internet_gateway" "igw" {
 
   tags = {
     Name = "main-igw"
+  }
+}
+
+resource "aws_subnet" "public" {
+  count                   = 1
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-subnet-${count.index}"
   }
 }
 
