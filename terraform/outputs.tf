2125c82231b3fd810c978d1b1bfd7f83581f5c2a
J4N06 <jan.birchler@iseag.ch>
2025-06-16 08:52:11 +0200

    output bearbeitet

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -5,6 +5,3 @@ output "instance_public_ips" {
 output "ssh_commands" {
   value = [for i in aws_instance.k8s_nodes : "ssh -i ~/.ssh/k8s/id_rsa ubuntu@${i.public_ip}"]
 }
-output "instance_public_ips" {
-  value = aws_instance.k8s_nodes[*].public_dns
-}
